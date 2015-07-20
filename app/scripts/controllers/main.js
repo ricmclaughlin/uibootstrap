227bff2b1061726fe7ab852dcd5e075f80c03b19
ric mclaughlin <ric_mclaughlin@yahoo.com>
2015-07-20 21:21:01 +0200

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name uibootstrapApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the uibootstrapApp
+ */
+angular.module('uibootstrapApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
