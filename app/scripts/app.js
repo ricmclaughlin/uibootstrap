7d4d71da7976aa9802dd65bed2bef16c227d0234
ric mclaughlin <ric_mclaughlin@yahoo.com>
2015-08-03 21:26:05 +0200

    add bootsketch and toolbox pages

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -30,35 +30,19 @@ angular
         controller: 'AboutCtrl',
         controllerAs: 'about'
       })
+      .when('/toolbox', {
+        templateUrl: 'views/toolbox.html',
+        controller: 'ToolboxCtrl',
+        controllerAs: 'toolbox'
+      })
+      .when('/bootsketch', {
+        templateUrl: 'views/bootsketch.html',
+        controller: 'BootsketchCtrl',
+        controllerAs: 'bootsketch'
+      })
       .otherwise({
         redirectTo: '/'
       });
   });
 
 
-angular.module('uibootstrapApp').controller('AccordionDemoCtrl', function ($scope) {
-  $scope.oneAtATime = true;
-
-  $scope.groups = [
-    {
-      title: 'Dynamic Group Header - 1',
-      content: 'Dynamic Group Body - 1'
-    },
-    {
-      title: 'Dynamic Group Header - 2',
-      content: 'Dynamic Group Body - 2'
-    }
-  ];
-
-  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
-
-  $scope.addItem = function() {
-    var newItemNo = $scope.items.length + 1;
-    $scope.items.push('Item ' + newItemNo);
-  };
-
-  $scope.status = {
-    isFirstOpen: true,
-    isFirstDisabled: false
-  };
-});
